bec3d320d398d76f52fdf128b26fd92295c506d3
Gerardo Vaquez <gerardovasquezac3@gmail.com>
2026-02-10 08:28:40 -0800

    Initial sharing of project

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+
+/**
+ * This is my first java app. It prints out Hello World 
+ * and my name to the console
+ *
+ * @author Gerardo Vasquez 
+ * @version v1.0
+ * @since 02/10/2026
+ */
+public class HelloWorld
+{
+ public static void main(String[]arg){
+     System.out.println("Hello World");
+     System.out.println("Gerardo Vasquez");
+ }
+}
